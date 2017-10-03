29bfaf188e82d8874a846abaa83bd209297513d9
Lucas Malta <lucas50xd@hotmail.com>
2017-10-03 17:33:17 -0300

    testing random network

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import _ from 'lodash';
 import './style.css';
+import Vis from '../node_modules/vis/dist/vis.js';
+// import '../node_modules/vis/dist/vis.css';
 
 function component() {
-  var element = document.createElement('div');
+   let element = document.createElement('div');
+  let nodes = new Vis.DataSet([
+    {id: 1, label: 'Node 1'},
+    {id: 2, label: 'Node 2'},
+    {id: 3, label: 'Node 3'},
+    {id: 4, label: 'Node 4'},
+    {id: 5, label: 'Node 5'}
+  ]);
 
-  // Lodash, now imported by this script
+  let edges = new Vis.DataSet([
+    {from: 1, to: 3},
+    {from: 1, to: 2},
+    {from: 2, to: 4},
+    {from: 2, to: 5}
+  ]);
+
+  let container = document.getElementById('mynetwork');
+
+  let data = {
+    nodes: nodes,
+    edges: edges
+  }
+
+  let options = {};
+
+  const network = new Vis.Network(container, data, options);
+//  Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
